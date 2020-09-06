95c68eb9799424a2123ddda44516d827c93e53aa
don-k-jacob <donkjacob.business@gmail.com>
2020-09-06 20:19:41 +0530

    updates

--- app/src/main/java/tech/tatos/aboutmekotlin/MainActivity.kt
+++ app/src/main/java/tech/tatos/aboutmekotlin/MainActivity.kt
@@ -1,12 +1,36 @@
 package tech.tatos.aboutmekotlin
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.textView)
+
+        nicknameTextView.text =editText.text
+        editText.visibility= View.GONE
+        view.visibility=View.GONE
+        nicknameTextView.visibility=View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
